5676ea5570c2d7700a50d2780ef2306d5c11f350
Shoomankhatri <shoomankhatri@gmail.com>
2023-12-27 21:05:00 +0545

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     { text1: "Give in to", text2: "your passions" },
   ]
 
-  const [heroCount, setHeroCount] = useState(1);
+  const [heroCount, setHeroCount] = useState(0);
   const [playStatus, setPlayStatus] = useState(false);
   return (
     <div>
