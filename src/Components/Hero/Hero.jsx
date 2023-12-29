19122e49c957cfebb331d72f66838fec07fb5a7c
Shoomankhatri <shoomankhatri@gmail.com>
2023-12-29 21:00:01 +0545

    edited

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,33 @@
 import './Hero.css';
-
-const Hero = ({herodData,setHeroCount,heroCount,setPlayStatus,playStatus}) => {
+import arrow_btn from "../../assets/arrow_btn.png";
+import play_icon from "../../assets/play_icon.png";
+import pause_icon from "../../assets/pause_icon.png";
+const Hero = ({ heroData, setHeroCount, heroCount, setPlayStatus, playStatus }) => {
     return (
         <div className='hero'>
-            <p>{herodData.text1}</p>
-            <p>{heroData.text2}</p>
+            <div className="hero-text">
+                <p>{heroData.text1}</p>
+                <p>{heroData.text2}</p>
             </div>
+
+            <div className="hero-explore">
+                <p>Explore the features.</p>
+                <img src={arrow_btn} alt="arrow_btn" />
+            </div>
+
+            <div className="hero-dot-play">
+                <ul className="hero-dots">
+                    <li onClick={() => setHeroCount(0)} className={heroCount === 0 ? "hero-dot orange" : "hero-dot"}></li>
+                    <li onClick={() => setHeroCount(1)} className={heroCount === 1 ? "hero-dot orange" : "hero-dot"}></li>
+                    <li onClick={() => setHeroCount(2)} className={heroCount === 2 ? "hero-dot orange" : "hero-dot"}></li>
+                </ul>
+            </div>
+
+            <div className="hero-play">
+                <img onClick={() => setPlayStatus(!playStatus)} src={playStatus ? pause_icon : play_icon} alt="" />
+                <p>See the Video</p>
+            </div>
+        </div>
     )
 }
 
